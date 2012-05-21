54a897b0859f6b39f495dcee472f87c790b77adb
Vladimir <vladimir0ne1@gmail.com>
2012-05-21 17:40:19 +0300

    дерево + стек + фукции

--- L2/tree.c
+++ L2/tree.c
@@ -1,5 +1,21 @@
-#include "tree.h"
+//#include "tree.h"
+#include "stdio.h"
+#include "stdlib.h"
 
+typedef struct tree_node
+{
+    char name[20];
+    int branch_num;
+    struct tree_node **branch;
+}
+tree_node;
+
+typedef struct stack
+{
+	char name[20];
+	struct stack* next;
+	struct tree_node* branch;
+}	stack;
 
 tree_node* create_list_and_add_to_stack(char node_name[20], stack** head)
 {
@@ -10,6 +26,7 @@ tree_node* create_list_and_add_to_stack(char node_name[20], stack** head)
 	nd->branch = NULL;
 	nd->branch_num = 0;
 	strcpy(nd->name, node_name);
+	strcpy((*head)->name, node_name);
 	(*head)->branch = nd;
 	return nd;
 }
@@ -20,6 +37,7 @@ tree_node* add_node_to_stack(char node_name[20], tree_node* node, stack** head)
 	work->next = *(head);
 	(*head) = work;	
 	(*head)->branch = node;
+	strcpy((*head)->name, node_name);
 	return node;
 }
 
@@ -43,8 +61,34 @@ tree_node* create_node(char node_name[20], int branch_count, stack** head)
 	(*head) = work; 
 	return nd;
 }
-	
-void main()
+void output_stack(stack* head)
 {
-	
+	puts("==== stack start: ====");
+	while(head)
+	{
+		puts(head->name);
+		head = head->next;
+	}
+	puts("==== stack end: ====");
+}
+void output(tree_node* s)
+{
+	static int level = 0;
+	if(s)
+	{
+		level++;
+		if (s->branch_num != 0)
+		{
+			output(s->branch[0]);
+			int i = 1;
+			while(i<(s->branch_num))
+			{
+				output(s->branch[i]);				
+				i++;
+			}
+		}
+		level--;
+		//puts(s->name);
+		printf("%s, level: %d\n", s->name, level);
+	}
 }
