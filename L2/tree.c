ebd29b7e684d2b0c0b9063bf27ef3e2ac7a1f3d2
Vladimir <vladimir0ne1@gmail.com>
2012-05-22 02:30:45 +0300

    добавлен стек для хранения количества операций в теле функции. Нужно для вложенности: цикл(свое тело функции), if-else(аналогично).

--- L2/tree.c
+++ L2/tree.c
@@ -17,6 +17,30 @@ typedef struct stack
 	struct tree_node* branch;
 }	stack;
 
+typedef struct action_num_stack
+{
+	int num;
+	struct action_num_stack* next;
+}	action_num_stack;
+
+int action_num_stack_pop(action_num_stack **head)
+{
+	action_num_stack* temp = (*head);
+	int x = temp->num;
+	(*head) = temp->next;
+	free(temp);
+	return x;
+}
+
+void action_num_stack_push(int x, action_num_stack **head)
+{
+	action_num_stack* temp = (*head);
+	temp = calloc(1, sizeof(action_num_stack));
+	temp->next = (*head);
+	temp->num = x;
+	(*head) = temp;
+}
+
 tree_node* create_list_and_add_to_stack(char node_name[20], stack** head)
 {
 	stack* work = calloc(1, sizeof(stack));
